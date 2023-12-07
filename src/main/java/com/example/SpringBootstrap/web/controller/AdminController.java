36ba8bc95ac5466c59fbc88461a8aad17d093b4a
Tomikus <tomikus1@gmail.com>
2023-12-07 20:04:48 +0300

    user edit added

--- src/main/java/com/example/SpringBootstrap/web/controller/AdminController.java
+++ src/main/java/com/example/SpringBootstrap/web/controller/AdminController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -25,7 +26,11 @@ public class AdminController {
     RoleService roleService;
 
     @GetMapping
-    public String printUsers(Model model) {
+    public String printUsers(@AuthenticationPrincipal User user, Model model) {
+        List<Role> roles = roleService.listRoles();
+        model.addAttribute("roles", roles);
+        model.addAttribute("user", user);
+        model.addAttribute("newUser", new User());
         model.addAttribute("users", userService.listUsers());
         return "admin";
     }
@@ -65,32 +70,20 @@ public class AdminController {
         return "redirect:/admin";
     }
 
-    @GetMapping("/user-update")
-    public String updateUserForm(@RequestParam("id") Long id, ModelMap model) {
-        List<Role> roles = roleService.listRoles();
-        model.addAttribute("roles", roles);
-        model.addAttribute("user", userService.findById(id));
-        return "user-update";
-    }
+//    @GetMapping("/user-update")
+//    public String updateUserForm(@RequestParam("id") Long id, ModelMap model) {
+//        List<Role> roles = roleService.listRoles();
+//        model.addAttribute("roles", roles);
+//        model.addAttribute("user", userService.findById(id));
+//        return "user-update";
+//    }
 
     @PostMapping("/user-update")
-    public ModelAndView updateUser(@Valid User user, BindingResult bindingResult, @RequestParam(value = "rolesChecked", required = false) String[] roles) {
-        ModelAndView modelAndView = new ModelAndView();
-        if (roles == null) {
-            bindingResult.rejectValue("roles", "rolesEmpty", "Roles can not be empty");
-        }
-
-        if (bindingResult.hasErrors()) {
-            modelAndView.addObject("roles", roleService.listRoles());
-            modelAndView.setViewName("user-update");
-            return modelAndView;
-        }
-
+    public String updateUser(User user, @RequestParam(value = "rolesChecked",
+            required = false) String[] roles, Model model) {
         Set<Role> roleSet = roleService.getRoleSetByName(roles);
         user.setRoles(roleSet);
         userService.update(user);
-
-        modelAndView.setViewName("redirect:/admin");
-        return modelAndView;
+        return "redirect:/admin";
     }
 }
