61f405f30f1479d73cd4689f3564053433c9ba87
Tomikus <tomikus1@gmail.com>
2023-12-07 17:53:30 +0300

    commented code removed

--- src/main/java/com/example/SpringBootstrap/web/controller/UserController.java
+++ src/main/java/com/example/SpringBootstrap/web/controller/UserController.java
@@ -23,42 +23,4 @@ public class UserController {
         model.addAttribute("user", userService.findById(user.getId()));
         return "user";
     }
-
-//    @GetMapping(value = "/{id}")
-//    public String userPage(@PathVariable("id") long id, Model model) {
-//        model.addAttribute("user", userService.findById(id));
-//        return "user";
-//    }
-
-//    @PostMapping("/user-add")
-//    public String addUser(@Valid User user, BindingResult bindingResult) {
-//        if (bindingResult.hasErrors()) {
-//            return "user-add";
-//        }
-//
-//        userService.add(user);
-//        return "redirect:/users";
-//    }
-
-//    @GetMapping("/user-delete")
-//    public String deleteUser(@RequestParam("id") Long id) {
-//        userService.delete(id);
-//        return "redirect:/users";
-//    }
-//
-//    @GetMapping("/user-update")
-//    public String updateUserForm(@RequestParam("id") Long id, Model model) {
-//        model.addAttribute("user", userService.findById(id));
-//        return "user-update";
-//    }
-//
-//    @PostMapping("/user-update")
-//    public String updateUser(@Valid User user, BindingResult bindingResult) {
-//        if (bindingResult.hasErrors()) {
-//            return "user-update";
-//        }
-//
-//        userService.update(user);
-//        return "redirect:/users";
-//    }
 }
