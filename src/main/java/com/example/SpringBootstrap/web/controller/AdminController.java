7f55a99d31107270161b14f9373d8871cc4e9394
Tomikus <tomikus1@gmail.com>
2023-12-08 18:08:16 +0300

    Bootstrap added

--- src/main/java/com/example/SpringBootstrap/web/controller/AdminController.java
+++ src/main/java/com/example/SpringBootstrap/web/controller/AdminController.java
@@ -4,14 +4,13 @@ import com.example.SpringBootstrap.web.model.Role;
 import com.example.SpringBootstrap.web.model.User;
 import com.example.SpringBootstrap.web.service.RoleService;
 import com.example.SpringBootstrap.web.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -38,52 +37,23 @@ public class AdminController {
         return "admin";
     }
 
-    @GetMapping("/user-add")
-    public String addUserForm(User user, ModelMap model) {
-        List<Role> roles = roleService.listRoles();
-        model.addAttribute("roles", roles);
-        return "user-add";
-    }
-
     @PostMapping("/user-add")
-    public ModelAndView addUser(@Valid User user, BindingResult bindingResult, @RequestParam(value = "rolesChecked", required = false) String[] roles) {
-        ModelAndView modelAndView = new ModelAndView();
-        if (roles == null) {
-            bindingResult.rejectValue("roles", "rolesEmpty", "Roles can not be empty");
-        }
-
-        if (bindingResult.hasErrors()) {
-            modelAndView.addObject("roles", roleService.listRoles());
-            modelAndView.setViewName("user-add");
-            return modelAndView;
-        }
-
+    public String addUser(@Valid User user, @RequestParam(value = "rolesChecked", required = false) String[] roles) {
         Set<Role> roleSet = roleService.getRoleSetByName(roles);
         user.setRoles(roleSet);
         userService.add(user);
 
-        modelAndView.setViewName("redirect:/admin");
-        return modelAndView;
+        return "redirect:/admin";
     }
 
-
-    @GetMapping("/user-delete")
+    @PostMapping("/user-delete")
     public String deleteUser(@RequestParam("id") Long id) {
         userService.delete(id);
         return "redirect:/admin";
     }
 
-//    @GetMapping("/user-update")
-//    public String updateUserForm(@RequestParam("id") Long id, ModelMap model) {
-//        List<Role> roles = roleService.listRoles();
-//        model.addAttribute("roles", roles);
-//        model.addAttribute("user", userService.findById(id));
-//        return "user-update";
-//    }
-
     @PostMapping("/user-update")
-    public String updateUser(User user, @RequestParam(value = "rolesChecked",
-            required = false) String[] roles, Model model) {
+    public String updateUser(User user, @RequestParam(value = "rolesChecked", required = false) String[] roles) {
         Set<Role> roleSet = roleService.getRoleSetByName(roles);
         user.setRoles(roleSet);
         userService.update(user);
