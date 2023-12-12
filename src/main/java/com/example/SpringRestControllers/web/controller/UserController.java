6650f518de7578702f594db74f9cfbaf8e6c785b
Tomikus <tomikus1@gmail.com>
2023-12-12 20:09:53 +0300

    unused import removed

--- src/main/java/com/example/SpringRestControllers/web/controller/UserController.java
+++ src/main/java/com/example/SpringRestControllers/web/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.example.SpringRestControllers.web.controller;
 
 import com.example.SpringRestControllers.web.model.User;
-import com.example.SpringRestControllers.web.service.UserService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
